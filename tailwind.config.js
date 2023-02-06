d945b28796b917624848fcd2a314544a3237a7ae
andres-acc <elkin.alcaraz@accenture.com>
2023-02-06 12:51:00 -0500

    Basic tailwind implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    colors: {
+      primary: '#00A0FF',
+      sky: '#E8F9FF',
+      ghost: '#F8F8FF',
+      grass: '#A5CD39',
+      black: '#000'
+    }
   },
   plugins: [],
 }
